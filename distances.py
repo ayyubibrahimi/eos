c767cc1a521a8aa5deabd7aad511d4363f4084e1
ayyubibrahimi <ayyub.ibrahimi@gmail.com>
2022-03-29 00:04:50 -0500

    calculated distances in yards for nearest neighbors

---
+++ distances.py
@@ -0,0 +1,31 @@
+import sys
+
+sys.path.append("../")
+import pandas as pd
+from sklearn.neighbors import BallTree
+import numpy as np
+
+
+def distance():
+    dfa = pd.read_csv("calls_for_service_3_21_2022_coordinates.csv")
+
+    dfb = pd.read_csv("new_orleans_cameras_3_11_2022.csv")
+
+    bt = BallTree(np.deg2rad(dfa[["latitude", "longitude"]].values), metric="haversine")
+    distances, indices = bt.query(np.deg2rad(np.c_[dfb["latitude"], dfb["longitude"]]))
+
+    l = []
+    for d in distances:
+        miles = d * 3958.8
+        yards = miles * 1760
+        l.append(yards)
+        df = pd.DataFrame(l)
+
+    # nearest call for service to camera: 16.67260112 is the avg distance in yards for the 432 known NOPD cameras
+
+    # nearest camera to a call for service: 350.3729814 is the avg distance in yards for the 79763 calls for service
+
+    # resource
+    # https://towardsdatascience.com/using-scikit-learns-binary-trees-to-efficiently-find-latitude-and-longitude-neighbors-909979bd929b
+
+    return df
